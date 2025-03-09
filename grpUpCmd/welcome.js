575fa31bf69d1e23b5d8dabb6b29ddc9e08c3043
hussainzx <hussainzx5x@gmail.com>
2025-03-09 12:29:26 +0000

    Update welcome.js

--- grpUpCmd/welcome.js
+++ grpUpCmd/welcome.js
@@ -4,7 +4,7 @@ module.exports = {
     const newMembers = event.participants;
     for (const member of newMembers) {
       await api.sendMessage(event.id, {
-        text: `Welcome @${member.split('@')[0]}!`,
+        text: `🎉 『 𝗪𝗘𝗟𝗖𝗢𝗠𝗘 』 🎉\n\n💠 𝗛𝗲𝘆 {userName}!\n🔹 𝗬𝗼𝘂 𝗷𝘂𝘀𝘁 𝗷𝗼𝗶𝗻𝗲𝗱 『 {boxName} 』\n⏳ 𝗧𝗶𝗺𝗲 𝗳𝗼𝗿 𝘀𝗼𝗺𝗲 𝗳𝘂𝗻! 𝗛𝗮𝘃𝗲 𝗮 𝗳𝗮𝗻𝘁𝗮𝘀𝘁𝗶𝗰 {session} 🎊\n\n⚠ 𝗣𝗹𝗲𝗮𝘀𝗲 𝗳𝗼𝗹𝗹𝗼𝘄 𝗮𝗹𝗹 𝗴𝗿𝗼𝘂𝗽 𝗿𝘂𝗹𝗲𝘀! 🚀`,
         mentions: [member]
       });
     }
